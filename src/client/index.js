526b9f535b505138c08417aa746922357c5450eb
Jefferson Garzon <jefferson.garzon@juvo.com>
2020-02-12 09:28:32 -0500

    wip

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
-const size = 500 //px
+const size = 700 //px
 const canvas = document.getElementById("canvas")
 const context = canvas.getContext("2d")
 
@@ -8,3 +8,84 @@ canvas.width = canvas.height = size
 //paint background
 context.fillColor = "#000"
 context.fillRect(0, 0, size, size)
+
+const initialState = {
+  ball: {
+    x: size / 2,
+    y: size / 2,
+    speed: { x: 1, y: 0 }
+  },
+  players: [
+    {
+      x: 0,
+      y: size / 2,
+      speed: { x: 0, y: 1.5 },
+      type: "vertical"
+    },
+    {
+      x: size,
+      y: size / 2,
+      speed: { x: 0, y: 1.5 },
+      type: "vertical"
+    }
+  ]
+}
+
+const updateWorld = (state, inputs) => {
+  const ball = updateBall(state.ball)
+  const players = state.players.map((p, index) =>
+    updatePlayer(p, inputs[index])
+  )
+
+  return {
+    ball,
+    players
+  }
+}
+
+const checkCollisions = state => {
+  return {
+    ball: checkBallCollisions(state.ball),
+    players: state.players.map(checkPlayerCollisions)
+  }
+}
+
+const updateBall = ball => {
+  return {
+    x: ball.x + ball.speed.x,
+    y: ball.y + ball.speed.y
+  }
+}
+
+const updatePlayer = (player, inputs) => {
+  return {
+    x: player.x + player.speed.x * inputs.horizontal,
+    y: player.y + player.speed.y * inputs.vertical
+  }
+}
+
+const checkBallCollisions = (ball) => {
+  const x, y;
+  const speed = { ...ball.speed }
+
+  if(ball.x <= 0) {
+    speed.x = 1
+    x = 0
+  }
+  else if(ball.x >= size) {
+    speed.x = -1
+    x = size
+  }
+
+  if(ball.y <= 0) {
+    speed.y = 1
+    y = 0
+  }
+  else if(ball.y >= size) {
+    speed.y = -1
+    y = size
+  }
+
+  return { x, y, speed }
+}
+
